bb7adc4e49dad540910a047d59a869f8e45a8ab1
Oksana Kurysheva <okurysheva@gmail.com>
2014-08-14 19:50:39 +0400

    limit number of members on the mail page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,21 @@ app.controller('MembersCtrl', ['$scope', '$http', function($scope, $http){
     $scope.members = [];
     $http.get('/assets/data/members.json').success(function(data) {
       $scope.members = data;
+    $scope.rankedList = [];
+    var n = 0;
+    var array = [];
+    while (n < 6) {
+        var index = Math.floor(Math.random()*$scope.members.length);
+        if ($.inArray(index, array) == -1) {
+            array[n+1] = index;
+            n++;
+        }
+    }
+    for (n=1; n < 7; n++) {
+       $scope.rankedList.push(
+            $scope.members[array[n]]
+        );
+    }
     });
   }]);
 
